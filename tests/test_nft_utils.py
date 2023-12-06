90c8a80e4334b26289c34eb3aa38ac1644642170
Tim Garrett <trg83@yahoo.com>
2023-12-06 15:17:05 -0600

    Work in progress to show Acevail

--- tests/test_nft_utils.py
+++ tests/test_nft_utils.py
@@ -7,11 +7,38 @@ MAX_BLOCK_COST_CLVM = DEFAULT_CONSTANTS.MAX_BLOCK_COST_CLVM
 from chia.types.blockchain_format.program import Program
 from chia.types.blockchain_format.sized_bytes import bytes32
 from chia.util.ints import uint64
+from chia.wallet.nft_wallet.nft_puzzles import (
+    NFT_METADATA_UPDATER,
+    NFT_STATE_LAYER_MOD_HASH
+)
 from chia.wallet.puzzles.load_clvm import load_clvm
+from chia.wallet.puzzles.singleton_top_layer_v1_1 import (
+    SINGLETON_LAUNCHER_HASH,
+    SINGLETON_MOD_HASH
+)
 
 LIST_OF_PAIRS_MOD: Program = load_clvm("test_list_of_pairs.clsp", package_or_requirement="clsp", recompile=True)
 
+CALCULATE_NFT_FULL_PUZZLE_HASH_MOD: Program = load_clvm("test_calculate_nft_full_puzzle_hash.clsp", package_or_requirement="clsp", recompile=True)
+
+NFT_METADATA_UPDATER_PUZZLE_HASH: bytes32 = NFT_METADATA_UPDATER.get_tree_hash()
+assert bytes32.from_hexstr("0xfe8a4b4e27a2e29a4d3fc7ce9d527adbcaccbab6ada3903ccf3ba9a769d2d78b") == NFT_METADATA_UPDATER_PUZZLE_HASH
+NFT_METADATA_UPDATER_PUZZLE_HASH_HASH: Program = Program.to(NFT_METADATA_UPDATER_PUZZLE_HASH).get_tree_hash()
+
 class TestNftUtils:
+    @pytest.mark.asyncio
+    async def test_calculate_nft_full_puzzle_hash(self):
+        result: Program = CALCULATE_NFT_FULL_PUZZLE_HASH_MOD.run([
+            SINGLETON_MOD_HASH,
+            SINGLETON_LAUNCHER_HASH,
+            NFT_STATE_LAYER_MOD_HASH,
+            bytes32.from_hexstr("0x059265da2dfa0dc477dd43ff9b4648d067341143ecaa3165b1e4202d07863dd1"), #METADATA_UPDATER_PUZZLE_HASH_HASH,
+            bytes32.from_hexstr("0xd9eb1eb2bd59d44e937e44dbfd9d70f8c593893eae1f86e363764401fa58d802"), #launcher ID
+            bytes32.from_hexstr("0xcb699f353e6066cdfd34abc003e1c029cbd34c069ed42f9233a95b9419058662"), #metadata hash
+            bytes32.from_hexstr("0x5bf47b4ac39c66c5fd2247a623acc22500254d2574d0b4496ec9ed36cd3c1847") # inner puzzlehash
+        ])
+        expected: Program = Program.to(bytes32.from_hexstr("0x2efafb3fd337152d1f9872508195e61a54cddb697e9f0a0e83a316d9e4e5ae95"))
+        assert expected == result
 
     @pytest.mark.asyncio
     async def test_list_of_pairs_empty(self):
